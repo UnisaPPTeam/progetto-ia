600530674f0a9d924c6d0becbc2c69ceffde0a9a
PGG106 <72303893+PGG106@users.noreply.github.com>
2023-11-18 06:48:25 +0100

    add a formula to calcualte the temporal difference

--- Racetrack.py
+++ Racetrack.py
@@ -1,16 +1,25 @@
 import gymnasium as gym
 from gymnasium import Space
-import numpy
+import numpy as np
 
-def pick_action(observation: numpy.ndarray, action_list: Space):
+def pick_action(observation: np.ndarray, action_list: Space):
     return action_list.sample()
 
+# Calculate the TD
+def get_temporal_difference(reward, gamma, new_q_values, old_q_value):
+    temporal_difference = reward + (gamma * np.max(new_q_values)) - old_q_value
 
 def main():
     env = gym.make('racetrack-v0', render_mode='rgb_array')
     observation, info = env.reset()
-
+    
+    # define Qlearning hyperparams
+    discount_factor = 0.9
+    learning_rate = 0.7
+    
     for _ in range(1000):
+        # We need to preserve the old observation to be sure we can access the old q value
+        old_observation = observation
         action_list = env.action_space
         action = pick_action(observation, action_list) # agent policy that uses the observation and info  
         observation, reward, terminated, truncated, info = env.step(action)
