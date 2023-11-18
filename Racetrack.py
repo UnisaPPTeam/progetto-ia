b82f7a7c2d3f736e9b6a2bf7cdb8ca1a15cc5845
PGG106 <72303893+PGG106@users.noreply.github.com>
2023-11-18 13:07:02 +0100

    actually return the temporal difference

--- Racetrack.py
+++ Racetrack.py
@@ -8,17 +8,32 @@ def pick_action(observation: np.ndarray, action_list: Space):
 # Calculate the TD
 def get_temporal_difference(reward, gamma, new_q_values, old_q_value):
     temporal_difference = reward + (gamma * np.max(new_q_values)) - old_q_value
+    return temporal_difference
 
 def main():
     env = gym.make('racetrack-v0', render_mode='rgb_array')
     # Set action space to discrete
-    env.configure({
-    "action": {
-        "type": "DiscreteAction"
-    }
-})
+    env.unwrapped.configure({
+        "observation": {
+            "type": "OccupancyGrid",
+            "features": ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"],
+            "features_range": {
+                "x": [-100, 100],
+                "y": [-100, 100],
+                "vx": [-20, 20],
+                "vy": [-20, 20]
+            },
+            "grid_size": [[-27.5, 27.5], [-27.5, 27.5]],
+            "grid_step": [5, 5],
+            "absolute": False,
+        },
+        "action": {
+            "type": "DiscreteAction"
+        }
+    })
     observation, info = env.reset()
-    print(env.action_space)
+    action_size = env.action_space.n
+    
     # define Qlearning hyperparams
     discount_factor = 0.9
     learning_rate = 0.7
